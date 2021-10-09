d3240d4d8ccef42d7b8593d3213a4e62c7c4f294
JosteveGit <adekanbijosteve@gmail.com>
2021-10-09 11:26:32 +0100

    :zap: Clean up

--- lib/utils/widgets/github.dart
+++ lib/utils/widgets/github.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GITHUB extends StatelessWidget {
   const GITHUB({
@@ -7,19 +8,27 @@ class GITHUB extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: Container(
-        padding: EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          shape: BoxShape.circle,
-        ),
-        child: Center(
-          child: Image.asset(
-            "assets/github.png",
-            width: 50,
-            fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () async {
+        String url = "";
+        if (await canLaunch(url)) {
+          launch(url);
+        }
+      },
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Container(
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: Image.asset(
+              "assets/github.png",
+              width: 50,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
